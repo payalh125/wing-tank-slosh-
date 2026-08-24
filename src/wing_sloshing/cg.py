4729344ac53b5c5e4f5c36396be5fd175216e3ad
PAYAL HELAMBE <101666148+payalh125@users.noreply.github.com>
2026-08-24 04:29:55 +0100

    Update cg.py

--- src/wing_sloshing/cg.py
+++ src/wing_sloshing/cg.py
@@ -1,1 +1,17 @@
+from .geometry import TankGeometry
+from .fuel import FuelState
 
+
+def calculate_fuel_cg(
+    geometry: TankGeometry,
+    fuel: FuelState
+):
+    x_cg = geometry.length / 2.0
+    y_cg = geometry.width / 2.0
+    z_cg = fuel.fuel_height(geometry) / 2.0
+
+    return {
+        "x_cg": x_cg,
+        "y_cg": y_cg,
+        "z_cg": z_cg
+    }
