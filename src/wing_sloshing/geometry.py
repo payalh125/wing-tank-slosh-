c907215055810ccef2d130dd699b48e80a0448fd
PAYAL HELAMBE <101666148+payalh125@users.noreply.github.com>
2026-08-24 04:28:46 +0100

    Update geometry.py

--- src/wing_sloshing/geometry.py
+++ src/wing_sloshing/geometry.py
@@ -1,1 +1,17 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class TankGeometry:
+    length: float
+    width: float
+    height: float
+    compartments: int
+
+    @property
+    def total_volume(self) -> float:
+        return self.length * self.width * self.height
+
+    @property
+    def compartment_length(self) -> float:
+        return self.length / self.compartments
