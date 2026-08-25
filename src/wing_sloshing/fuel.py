67ae15437460666bb6e52ec18250c7762531bb2e
PAYAL HELAMBE <101666148+payalh125@users.noreply.github.com>
2026-08-25 10:51:30 +0100

    Update fuel.py

--- src/wing_sloshing/fuel.py
+++ src/wing_sloshing/fuel.py
@@ -1,18 +1,13 @@
-from dataclasses import dataclass
+class Fuel:
+    """
+    Fuel properties and basic mass calculations.
+    """
 
-from .geometry import TankGeometry
+    def __init__(self, density):
+        self.density = density
 
+    def mass_from_volume(self, volume):
+        return self.density * volume
 
-@dataclass
-class FuelState:
-    density: float
-    fill_fraction: float
-
-    def volume(self, geometry: TankGeometry) -> float:
-        return geometry.total_volume * self.fill_fraction
-
-    def mass(self, geometry: TankGeometry) -> float:
-        return self.density * self.volume(geometry)
-
-    def fuel_height(self, geometry: TankGeometry) -> float:
-        return geometry.height * self.fill_fraction
+    def volume_from_mass(self, mass):
+        return mass / self.density
