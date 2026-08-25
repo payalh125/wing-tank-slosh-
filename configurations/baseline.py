a47198eb9a06597f9ba862b8962b723e45fc99a4
PAYAL HELAMBE <101666148+payalh125@users.noreply.github.com>
2026-08-25 12:49:12 +0100

    Update baseline.py

--- configurations/baseline.py
+++ configurations/baseline.py
@@ -15,17 +15,44 @@ GRAVITY = 9.81
 
 
 
-
 BAFFLE_LOCATION = 0.600
 
-# Modelling assumptions
 OPENING_AREA = 0.010
 DISCHARGE_COEFFICIENT = 0.600
 
 
 
+
 NUM_X = 1000
+
 
 FILL_FRACTIONS = [0.30, 0.60, 0.90]
 
-ACCELERATIONS_G = [-0.50, -0.25, 0.0, 0.25, 0.50]
+ACCELERATIONS_G = [
+    -0.50,
+    -0.25,
+    0.00,
+    0.25,
+    0.50
+]
+
+
+# Pressure sensor locations
+
+
+PRESSURE_SENSORS = {
+    "P1": {"x": 0.00, "z": 0.00},
+    "P2": {"x": 0.24, "z": 0.00},
+    "P3": {"x": 0.48, "z": 0.00},
+    "P4": {"x": 0.72, "z": 0.00},
+    "P5": {"x": 0.96, "z": 0.00},
+    "P6": {"x": 1.20, "z": 0.00},
+}
+
+
+
+TRANSIENT_FILL_FRACTION = 0.30
+TRANSIENT_ACCELERATION_G = -0.50
+
+TRANSIENT_DURATION = 10.0
+TRANSIENT_POINTS = 250
